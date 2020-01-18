ccb81ea66fa10b56d978c5369d146fe968d26c3e
Marcin Polak <mapoart@gmail.com>
2020-01-18 19:25:39 +0100

    Added one more error solution for classname/filename issues

--- nexss.haxe.errors.js
+++ nexss.haxe.errors.js
@@ -1,8 +1,6 @@
 module.exports = {
-  'Package "(.*?)" was not found in any of class paths':
-    "nexss hx install <module>",
-  "Class name must start with uppercase character":
-    "Your filename must starts with uppercase character for HAXE language. Please rename it to eg. Text.hx OR MyFile.hx",
-  "Module My does not define type (.*?)":
-    "Rename your class to your program name eg. if program name is called MyClass.hx you need to change the class name to 'MyClass'"
+    'Package "(.*?)" was not found in any of class paths': "nexss hx install <module>",
+    "Class name must start with an uppercase letter": "Make sure that Filename and Class inside start with the uppercase letter and are the same.",
+    "Class name must start with uppercase character": "Your filename must starts with uppercase character for HAXE language. Please rename it to eg. Text.hx OR MyFile.hx",
+    "Module My does not define type (.*?)": "Rename your class to your program name eg. if program name is called MyClass.hx you need to change the class name to 'MyClass'"
 };
