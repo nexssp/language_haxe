46b65dcf7fd7102e4b9653a88884a4d5b7c909b9
Marcin Polak <mapoart@gmail.com>
2020-09-13 17:06:15 +0200

    Added Distros implementation

--- haxe.linux.nexss.config.js
+++ haxe.linux.nexss.config.js
@@ -1,13 +1,34 @@
 let languageConfig = Object.assign({}, require("./haxe.win32.nexss.config"));
+let sudo = "sudo ";
+if (process.getuid && process.getuid() === 0) {
+  sudo = "";
+}
 languageConfig.compilers = {
   haxe: {
-    install: "scoop install haxe",
+    install: `${sudo}apt install -y haxe`,
     command: "haxe",
     // args: "-main <file> --interp",
     args: "--run <fileNoExt>",
     help: ``,
   },
 };
+
+const {
+  replaceCommandByDist,
+  dist,
+} = require(`${process.env.NEXSS_SRC_PATH}/lib/osys`);
+
+const distName = dist();
+languageConfig.dist = distName;
+
+// TODO: Later to cleanup this config file !!
+switch (distName) {
+  default:
+    languageConfig.compilers.haxe.install = replaceCommandByDist(
+      languageConfig.compilers.haxe.install
+    );
+    break;
+}
 
 languageConfig.languagePackageManagers = {
   haxelib: {
